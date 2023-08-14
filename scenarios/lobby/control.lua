3062df186547831f02d754ff5fe7b7dbc366ccef
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2023-08-14 12:06:57 -0700

    spawn enemies around player in arena

--- scenarios/lobby/control.lua
+++ scenarios/lobby/control.lua
@@ -966,6 +966,23 @@ local function on_tick(event)
             global.healing_players[id] = nil
         end
     end
+
+    if global.game_state == "arena" then
+        local difficulties = {
+            easy = 2,
+            normal = 1,
+            hard = 0.5,
+        }
+        local balance = difficulties[global.lobby_options.difficulty] * 60
+        if game.tick % balance == 0 then
+            for _, player in pairs(game.connected_players) do
+                local radius = math.random(25, 50)
+                local position = get_random_position_on_circumference(player.position, radius)
+                local enemy_name = "small-biter"
+                spawn_new_enemy(player.surface, position, enemy_name, player)
+            end
+        end
+    end
 end
 
 script.on_init(on_init)
