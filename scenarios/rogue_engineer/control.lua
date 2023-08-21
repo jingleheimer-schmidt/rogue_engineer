13e3f06a1f2ba74df12e27ef6ea1e674fa954d4d
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2023-08-21 13:38:23 -0700

    actually remove the selected ability from the options

--- scenarios/rogue_engineer/control.lua
+++ scenarios/rogue_engineer/control.lua
@@ -81,7 +81,7 @@ local function randomize_starting_abilities()
     local available_abilities = util.table.deepcopy(global.available_abilities)
     local default_abilities = global.default_abilities
     local starting_ability = global.lobby_options.starting_ability
-    available_abilities[default_abilities[starting_ability]] = false
+    available_abilities[default_abilities[starting_ability]] = nil
     for ability_number, ability_name in pairs(default_abilities) do
         if not (ability_number == starting_ability) then
             local random_ability = random_table_key(available_abilities)
