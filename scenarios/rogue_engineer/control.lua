ab543a6b4bfd8485042d93fb8652feb7fb14657e
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2023-08-30 16:24:30 -0700

    fix healing amount

--- scenarios/rogue_engineer/control.lua
+++ scenarios/rogue_engineer/control.lua
@@ -966,8 +966,8 @@ local function unlock_named_ability(ability_name, player)
         global.healing_players = global.healing_players or {}
         global.healing_players[player.index] = {
             player = player,
-            damage = player.character.prototype.max_health / 15,
-            final_tick = game.tick + 60 * 15,
+            damage = - player.character.prototype.max_health / (60 * 15),
+            final_tick = game.tick + (60 * 15),
         }
     end
 end
