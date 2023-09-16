64b13606529a3d26df952d6eb656f456c1114321
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2023-09-15 20:58:56 -0700

    temp validation file to data-dump into for formatting

---
+++ validation.lua
@@ -0,0 +1,154 @@
+local abilities = {
+    barrier = {
+        cooldown_at_max_upgrade = 831,
+        cooldown_upgrade_count = 19,
+        damage_at_max_upgrade = 0,
+        damage_upgrade_count = 0,
+        radius_at_max_upgrade = 20,
+        radius_upgrade_count = 10
+    },
+    beam_blast = {
+        cooldown_at_max_upgrade = 315,
+        cooldown_upgrade_count = 12,
+        damage_at_max_upgrade = 80,
+        damage_upgrade_count = 4,
+        radius_at_max_upgrade = 20,
+        radius_upgrade_count = 5
+    },
+    burst = {
+        cooldown_at_max_upgrade = 552,
+        cooldown_upgrade_count = 12,
+        damage_at_max_upgrade = 187.5,
+        damage_upgrade_count = 10,
+        radius_at_max_upgrade = 5.5,
+        radius_upgrade_count = 9
+    },
+    crystal_blossom = {
+        cooldown_at_max_upgrade = 195,
+        cooldown_upgrade_count = 9,
+        damage_at_max_upgrade = 180,
+        damage_upgrade_count = 6,
+        radius_at_max_upgrade = 13.5,
+        radius_upgrade_count = 10
+    },
+    cure = {
+        cooldown_at_max_upgrade = 600,
+        cooldown_upgrade_count = 4,
+        damage_at_max_upgrade = -2.8125,
+        damage_upgrade_count = 10,
+        radius_at_max_upgrade = 1,
+        radius_upgrade_count = 0
+    },
+    defender = {
+        cooldown_at_max_upgrade = 1460,
+        cooldown_upgrade_count = 20,
+        damage_at_max_upgrade = 0,
+        damage_upgrade_count = 0,
+        radius_at_max_upgrade = 1,
+        radius_upgrade_count = 0
+    },
+    destroyer = {
+        cooldown_at_max_upgrade = 3420,
+        cooldown_upgrade_count = 20,
+        damage_at_max_upgrade = 0,
+        damage_upgrade_count = 0,
+        radius_at_max_upgrade = 1,
+        radius_upgrade_count = 0
+    },
+    discharge_defender = {
+        cooldown_at_max_upgrade = 1628,
+        cooldown_upgrade_count = 14,
+        damage_at_max_upgrade = 0,
+        damage_upgrade_count = 0,
+        radius_at_max_upgrade = 8,
+        radius_upgrade_count = 0
+    },
+    distractor = {
+        cooldown_at_max_upgrade = 1560,
+        cooldown_upgrade_count = 20,
+        damage_at_max_upgrade = 0,
+        damage_upgrade_count = 0,
+        radius_at_max_upgrade = 10,
+        radius_upgrade_count = 0
+    },
+    gun_turret = {
+        cooldown_at_max_upgrade = 2451,
+        cooldown_upgrade_count = 9,
+        damage_at_max_upgrade = 0,
+        damage_upgrade_count = 0,
+        radius_at_max_upgrade = 35,
+        radius_upgrade_count = 5
+    },
+    landmine = {
+        cooldown_at_max_upgrade = 308,
+        cooldown_upgrade_count = 13,
+        damage_at_max_upgrade = 0,
+        damage_upgrade_count = 0,
+        radius_at_max_upgrade = 3,
+        radius_upgrade_count = 4
+    },
+    pavement = {
+        cooldown_at_max_upgrade = 83,
+        cooldown_upgrade_count = 16,
+        damage_at_max_upgrade = 0,
+        damage_upgrade_count = 0,
+        radius_at_max_upgrade = 4,
+        radius_upgrade_count = 3
+    },
+    poison_capsule = {
+        cooldown_at_max_upgrade = 2160,
+        cooldown_upgrade_count = 8,
+        damage_at_max_upgrade = 0,
+        damage_upgrade_count = 0,
+        radius_at_max_upgrade = 38,
+        radius_upgrade_count = 4
+    },
+    punch = {
+        cooldown_at_max_upgrade = 150,
+        cooldown_upgrade_count = 6,
+        damage_at_max_upgrade = 195,
+        damage_upgrade_count = 13,
+        radius_at_max_upgrade = 6.5,
+        radius_upgrade_count = 5
+    },
+    purifying_light = {
+        cooldown_at_max_upgrade = 840,
+        cooldown_upgrade_count = 12,
+        damage_at_max_upgrade = 0,
+        damage_upgrade_count = 0,
+        radius_at_max_upgrade = 54,
+        radius_upgrade_count = 12
+    },
+    rocket_launcher = {
+        cooldown_at_max_upgrade = 60,
+        cooldown_upgrade_count = 30,
+        damage_at_max_upgrade = 0,
+        damage_upgrade_count = 0,
+        radius_at_max_upgrade = 45,
+        radius_upgrade_count = 6
+    },
+    shotgun = {
+        cooldown_at_max_upgrade = 100,
+        cooldown_upgrade_count = 20,
+        damage_at_max_upgrade = 0,
+        damage_upgrade_count = 0,
+        radius_at_max_upgrade = 35,
+        radius_upgrade_count = 5
+    },
+    slash = {
+        cooldown_at_max_upgrade = 190,
+        cooldown_upgrade_count = 7,
+        damage_at_max_upgrade = 140,
+        damage_upgrade_count = 14,
+        radius_at_max_upgrade = 13.75,
+        radius_upgrade_count = 7
+    },
+    slowdown_capsule = {
+        cooldown_at_max_upgrade = 1380,
+        cooldown_upgrade_count = 8,
+        damage_at_max_upgrade = 0,
+        damage_upgrade_count = 0,
+        radius_at_max_upgrade = 30,
+        radius_upgrade_count = 4
+    }
+}
